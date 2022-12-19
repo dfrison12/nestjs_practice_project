f271521a90ef4ec82e00b0812a8a9021c6dd5b08
dfrison12 <dariohfrison@gmail.com>
2022-12-19 11:39:48 -0300

    inyectar servicios en otros servicios

--- 02-car-dealership/src/seed/seed.service.ts
+++ 02-car-dealership/src/seed/seed.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CARS_SEED } from './cars.seed';
 import { BRANDS_SEED } from './brands.seed';
+import { CarsService } from '../cars/cars.service';
+import { BrandsService } from '../brands/brands.service';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly cardService: CarsService,
+    private readonly brandService: BrandsService,
+  ) {}
+
   populateDB() {
+    this.cardService.fillCarsWithSeedData(CARS_SEED);
+    this.brandService.fillBrandsWithSeedData(BRANDS_SEED);
+
     return 'SEED executed successfully';
   }
 }
